5f48f2d5944a7aa9c7a9dd5162a87912f46434a0
Sangyun Shin <sangyun0914@g.skku.edu>
2023-08-01 10:01:22 +0900

    Add huber+CE loss

--- core/trainers/TrainerPlusCurriculum.py
+++ core/trainers/TrainerPlusCurriculum.py
@@ -1,5 +1,10 @@
-from transformers import Trainer
 from typing import Dict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import Trainer
+
 
 class CurriculumTrainer(Trainer):
   def __init__(self, *args, **kwargs):
@@ -49,3 +54,26 @@ class CurriculumTrainer(Trainer):
     output = {**logs, **{"step": self.state.global_step}}
     self.state.log_history.append(output)
     self.control = self.callback_handler.on_log(self.args, self.state, self.control, logs)
+
+  def compute_loss(self, model, inputs, return_outputs=False):
+      labels = inputs.get("labels")
+      logits = model(**inputs).logits
+      max_logits_indices = torch.argmax(logits, dim=2)
+
+      weight = torch.ones(logits.size(2))  # Initialize with weight 1 for all classes
+      weight[32500:33001] = 0  # Set weight 0 for the specified range [33000, 32500]
+
+      loss_fct = nn.CrossEntropyLoss(weight=weight, ignore_index=-100)
+
+      # Compute Cross Entropy (CE) loss for all elements in the logits and labels tensor.
+      ce_loss = loss_fct(logits.view(-1, logits.size(-1)), labels.view(-1))
+
+      # Mask for elements if <loc_{idx}> in range [33000, 32500] inclusive
+      mask_mse = ((labels >= 32500) & (labels <= 33000)) & ((max_logits_indices >= 32500) & (max_logits_indices <= 33000))
+
+      huber_loss = F.smooth_l1_loss(max_logits_indices[mask_mse].float(), labels[mask_mse].float())
+
+      loss = ce_loss + huber_loss
+
+      return (loss, logits) if return_outputs else loss
+
