c20cf7e382684bf19ffc41fa476e1aaf326a881d
Sangyun Shin <sangyun0914@g.skku.edu>
2023-09-06 13:12:45 +0900

    Update API for blank image

--- api.py
+++ api.py
@@ -1,15 +1,21 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
+import torch
 from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
+from PIL import Image
 from transformers import HfArgumentParser
 
+from core.common.utils import get_visual_bbox, img_trans_torchvision
 from core.models import (UdopConfig, UdopTokenizer,
                          UdopUnimodelForConditionalGeneration)
 
 app = Flask(__name__)
 CORS(app)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 @dataclass
 class Arguments:
@@ -85,18 +91,73 @@ model = UdopUnimodelForConditionalGeneration.from_pretrained(
 def index():
     return 'UDOP API'
 
-
+# TODO: Add padding to input_ids, token_boxes
 @app.route('/main', methods=['POST'])
 def main():
     response = Response()
     if request.method == 'POST':
         response.headers.add("Access-Control-Allow-Origin", "*")
         data = request.get_json()
-        print(data)
 
+        # text: List[str]]
+        # Assumes text is given as a list of sentences
+        # ex) text = ['Sentence 1', 'Sentence 2']
+        text = data['text']
 
+        sentinel_idx = 0
+        masked_text = []
+        word_tokens = []
+        task_boxes = []
+        token_boxes = []
 
+        for sentence in text:
+            masked_text.append(f'<extra_l_id_{sentinel_idx}>')
+            masked_text.append(sentence)
+            masked_text.append(f'</extra_l_id_{sentinel_idx}>')
 
+            if sentinel_idx > 100:
+                break
+
+            sentinel_idx += 1
+
+        task_tokens = tokenizer.tokenize(args.task_name + ".")
+        task_boxes = [[0,0,0,0] for _ in range(len(task_tokens))]
+        for text in masked_text:
+            word_tokens.extend(token for token in (tokenizer.tokenize(text)))
+        token_boxes = [[0,0,0,0] for _ in range(len(word_tokens))]
+
+        task_ids = tokenizer.convert_tokens_to_ids(task_tokens)
+        word_ids = tokenizer.convert_tokens_to_ids(word_tokens)
+
+        # Prepare model inputs
+        input_ids = task_ids + word_ids
+        seg_data = task_boxes + token_boxes
+        attention_mask = [1] * len(input_ids)
+        image = Image.new('RGB', (224, 224), 'rgb(0, 0, 0)') 
+        image = img_trans_torchvision(image, 224)
+        visual_seg_data = get_visual_bbox() 
+
+        # Process and batch inputs
+        input_ids = torch.tensor(input_ids, dtype=torch.long).unsqueeze(0).to(device)
+        seg_data = torch.tensor(seg_data, dtype=torch.float).unsqueeze(0).to(device)
+        attention_mask = torch.tensor(attention_mask, dtype=torch.long).unsqueeze(0).to(device)
+        image = image.unsqueeze(0).to(device)
+        visual_seg_data = visual_seg_data.unsqueeze(0).to(device)
+
+        # Generate output
+        output_ids = model.generate(
+                                input_ids,
+                                seg_data=seg_data,
+                                image=image,
+                                visual_seg_data=visual_seg_data,
+                                use_cache=True,
+                                decoder_start_token_id=tokenizer.pad_token_id,
+                                num_beams=1,
+                                max_length=512,
+                            )
+
+        prediction_text = tokenizer.decode(output_ids[0][1:-1])
+        print(prediction_text)
 
 
 
